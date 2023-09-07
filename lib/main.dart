979974e7757cafe622bdf1f99c04568fbf735179
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-09-07 13:34:52 +0200

    Färdig med vecka 1
    UI klar

--- lib/main.dart
+++ lib/main.dart
@@ -13,113 +13,195 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Todo {
+  final String item;
+  final String desc;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Todo(this.item, this.desc);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    List<Todo> todolist = [
+      Todo('Handla', 'Morötter, mjölk och paprika'),
+      Todo('Handla2', 'Morötter, mjölk och paprika'),
+      Todo('Handla3', 'Morötter, mjölk och paprika'),
+      Todo('Handla4', 'Morötter, mjölk och paprika'),
+      Todo('Handla5', 'Morötter, mjölk och paprika'),
+      Todo('Handla6', 'Morötter, mjölk och paprika'),
+      Todo('Handla7', 'Morötter, mjölk och paprika'),
+      Todo('Handla8', 'Morötter, mjölk och paprika'),
+      Todo('Handla9', 'Morötter, mjölk och paprika'),
+      Todo('Handla10', 'Morötter, mjölk och paprika'),
+      Todo('Handla11', 'Morötter, mjölk och paprika'),
+      Todo('Handla12', 'Morötter, mjölk och paprika'),
+      Todo('Handla', 'Morötter, mjölk och paprika'),
+      Todo('Handla', 'Morötter, mjölk och paprika'),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        title: const Text('To-Do'),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (String value) {},
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'done',
+                child: Text('Done'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'notDone',
+                child: Text('Not Done'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'all',
+                child: Text('All'),
+              ),
+            ],
+          ),
+        ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
+      bottomNavigationBar: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AddView()));
+          },
+          child: const Icon(Icons.add, color: Colors.black87,size: 38, ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+                child: ListView.builder(
+              itemBuilder: (context, index) {
+                return TodoList(todolist[index]);
+              },
+              itemCount: todolist.length,
+            )),
+          ],
+        ),
+      ),
+    );
+    // This trailing comma makes auto-formatting nicer for build method
+  }
+}
+
+class TodoList extends StatelessWidget {
+  final Todo todo;
+  const TodoList(this.todo, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => OtherView(todo)));
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey, // Border color
+              width: 0.5, // Border width
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        padding: const EdgeInsets.all(10.0), // Padding inside the container
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(right: 10, top: 2),
+              child: Icon(Icons.check_box_outline_blank),
+            ),
+            Expanded(
+              child: Text(todo.item,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  )),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.delete),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class OtherView extends StatelessWidget {
+  final Todo todo;
+  const OtherView(this.todo, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(todo.item)),
+      body: Column(
+        children: [
+          Text(todo.desc),
+        ],
+      ),
+    );
+  }
+}
+
+class AddView extends StatelessWidget {
+  const AddView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add to list:'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.rectangle,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  margin: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Theme.of(context).colorScheme.inversePrimary,
+                      hintStyle: const TextStyle(fontSize: 17),
+                      hintText: 'Add:',
+                      suffixIcon: const Icon(Icons.add),
+                      contentPadding: const EdgeInsets.all(20),
+                    ),
+                  ),
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
