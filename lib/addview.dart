e575d24c256797d72f884f70885fd1e0c0fe1b8a
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-09-28 10:38:31 +0200

    ändrade tillbaka import mystate till MyState i addview
    Jag har försökt ändra filnamnet på i libraryt till enbart små bokstäver. Ändå varje gång jag byter branch tillbaka till main får jag tillbaka MyState istället för mystate, vilket pajar en import till addview. så nu får det vara MyState..

--- lib/addview.dart
+++ lib/addview.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'mystate.dart';
+import 'MyState.dart';
 import 'todoitem.dart';
 
 class AddView extends StatelessWidget {
